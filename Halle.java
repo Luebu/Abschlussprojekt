1b145863f9eb96f3fcf443c046a521c9cc3ccf7e
Akomi03 <55061661+Akomi03@users.noreply.github.com>
2022-02-03 15:35:40 +0100

    initial commit

--- Halle.java
+++ Halle.java
@@ -1,2 +1,26 @@
 public class Halle {
+    private Verein mieterliste;
+    private double platz;
+
+    public void mieten(pVerein Verein){
+
+    }
+    public void freigeben(pVerein Verein) {
+
+    }
+
+
+    public double getPlatz() {
+        return platz;
+    }
+    public Verein getMieterliste() {
+        return mieterliste;
+    }
+    public void setMieterliste(Verein pMieterliste) {
+        mieterliste = pMieterliste;
+    }
+    public void setPlatz(double pPlatz) {
+        platz = pPlatz;
+    }
 }
+
